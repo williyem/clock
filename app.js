59060b4dd29418aac565b09980ecdf2dca616a21
Williyem <hinnehwilliams@gmail.com>
2021-07-14 19:46:49 +0000

    Done

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const secondHand = document.querySelector("[data-second-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const hourHand = document.querySelector("[data-hour-hand]");
 
-
+updateTime();
 setInterval(updateTime,1000);
 
 
 function updateTime(){
    let date = new Date();
-   secondHand.style.setProperty("--rotation", date.getSeconds()*6 +"deg");
-   minuteHand.style.setProperty("--rotation", date.getMinutes()*6+"deg");
-   hourHand.style.setProperty("--rotation",date.getMinutes() + date.getHours()*30+"deg");
+   minuteHand.style.setProperty("--rotation", date.getMinutes()*6);
+   hourHand.style.setProperty("--rotation",date.getMinutes() + date.getHours()*30);
+   secondHand.style.setProperty("--rotation", date.getSeconds()*6);
 }
-    
-
